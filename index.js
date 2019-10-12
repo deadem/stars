398803fac6e628cce0662ab599528c6f1e7b808d
deadem <deadem@gmail.com>
2019-10-12 20:51:44 +0300

    добавлен зум и респавн
    (cherry picked from commit 20bc7d506ed4e1b3ab1e3667af02f0614165baf4)

--- index.js
+++ index.js
@@ -22,15 +22,18 @@ function preload ()
 function create () {
     graphics = this.add.graphics();
 
-    for (let i = 0; i < 2000; ++i) {
+    for (let i = 0; i < 1000; ++i) {
         stars.push({
             r: Math.random() * width,
             a: Math.random() * Math.PI * 2,
-            d: (2 - Math.random() * 2) / 30,
+            d: (1 - Math.random() * 2) / 30,
+            z: 2 + Math.random() * 5,
             rect: this.add.rectangle(0, 0, 4, 4, 0xffffff)});
     }
 
 }
+
+let tick = 0;
 
 function update () {
     let centerX = width / 2;
@@ -41,8 +44,15 @@ function update () {
         let y = v.r * Math.cos(v.a);
 
         v.a += v.d;
+        v.r += v.z;
+
+        if (v.r > width) {
+            v.r = Math.random() * 100;
+        }
 
         v.rect.setPosition(centerX + x, centerY + y);
+        v.rect.setAlpha(0.5 + v.r / width * Math.sin(tick));
 
     });
+    tick = tick + 0.1;
 }
